bd42fa905660fe8fbd625be6d64025ba16f0dc68
yomarsuarez <yomaralveiro.suarez@gmail.com>
2025-09-18 09:56:52 -0500

    fixing auth

--- task-frontend/src/views/auth/ConfirmAccountView.tsx
+++ task-frontend/src/views/auth/ConfirmAccountView.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { PinInput, PinInputField } from "@chakra-ui/pin-input";
 import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
@@ -9,6 +9,7 @@ import { toast } from "react-toastify";
 export default function ConfirmAccountView() {
   const [token, setToken] = useState<ConfirmToken["token"]>("");
 
+  const navigate = useNavigate();
   const { mutate } = useMutation({
     mutationFn: confirmAccount,
     onError: (error) => {
@@ -16,6 +17,7 @@ export default function ConfirmAccountView() {
     },
     onSuccess: (data) => {
       toast.success(data);
+      navigate("/auth/login");
     },
   });
 
